45900685b731c377e950b1081e9cfdde1418708f
Steven_Klavins <stevenklavins94@gmail.com>
2021-04-05 17:33:12 +0100

    chore: added code comments

--- space-shooter.py
+++ space-shooter.py
@@ -2,15 +2,19 @@
 import pygame
 import sys
 
+# Initialize pygame at 720P
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 
+# Start game loop
 while True:
     for event in pygame.event.get():
+        # Allow player to quit and close window
         if event.type == pygame.QUIT:
             pygame.quit
             sys.exit()
             
     pygame.display.update()
+    # Set frame rate
     clock.tick(120)
