b8d9bb1c27c79b2b693bb9e8055b5db2b820a2ff
Steven_Klavins <stevenklavins94@gmail.com>
2021-04-08 23:43:42 +0100

    feat: added spaceship sprite group

--- space-shooter.py
+++ space-shooter.py
@@ -2,10 +2,23 @@
 import pygame
 import sys
 
+class SpaceShip(pygame.sprite.Sprite):
+    def __init__(self, path, x_pos, y_pos, speed):
+        super().__init__()
+        self.image = pygame.image.load(path)
+        self.rect = self.image.get_rect(center = (x_pos, y_pos))
+
 # Initialize pygame at 720P
 pygame.init()
 screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
+
+# Create spaceship from spaceship class
+spaceship = SpaceShip('spaceship.png', 640, 500, 10)
+
+# Add spaceship to a single group
+spaceship_group = pygame.sprite.GroupSingle()
+spaceship_group.add(spaceship)
 
 # Start game loop
 while True:
@@ -15,7 +28,12 @@ while True:
             pygame.quit
             sys.exit()
             
-    screen.fill((42,45,51))        
+    # Set background colour      
+    screen.fill((42,45,51))  
+    
+    # Render spacship
+    spaceship_group.draw(screen)     
+          
     pygame.display.update()
     # Set frame rate
     clock.tick(120)
